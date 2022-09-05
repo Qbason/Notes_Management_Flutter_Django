85ad98c29adb529629b4b187ba020288ffb0d5f0
Jakub K <kuba1095@gmail.com>
2022-09-05 12:16:52 +0200

    changed serializer

--- notes/api/serializers.py
+++ notes/api/serializers.py
@@ -19,15 +19,3 @@ class UserSerializer(ModelSerializer):
     class Meta:
         model = User
         fields = ['id','username','password','email']
-
-    def create(self, validated_data):
-        user =  User.objects.create(
-            username = validated_data['username'],
-            email = validated_data['email']   
-        )
-        user.set_password(
-            validated_data['password']
-        )
-        user.save()
-
-        return user
